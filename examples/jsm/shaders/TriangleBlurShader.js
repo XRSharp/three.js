7a3c64f80ad18d0e1614f2cf1de5562a73216392
giovanni-a <8248552+giovanni-a@users.noreply.github.com>
2024-03-12 17:32:47 +0100

    Progress

--- examples/jsm/shaders/TriangleBlurShader.js
+++ examples/jsm/shaders/TriangleBlurShader.js
@@ -21,6 +21,8 @@ const TriangleBlurShader = {
 
 		'tDiffuse': { value: null },
 		'sigma': { value: 4.0 },
+		'expectedMin': { value: 0.0 },
+		'expectedMax': { value: 0.8 },
 		'delta': { value: new Vector2(0.002, 0.002) }
 
 	},
@@ -50,6 +52,8 @@ const TriangleBlurShader = {
 		uniform sampler2D tDiffuse;
 		uniform vec2 delta; // sampling radius
 		uniform float sigma;
+		uniform float expectedMin;
+		uniform float expectedMax;
 
 		varying vec2 vUv;
 
@@ -58,36 +62,44 @@ const TriangleBlurShader = {
         }
 
 		bool isInfluencingCurrentSample(float sampleDistance, float sampleDepth) {
-            return sampleDepth > sampleDistance * 0.5 && sampleDepth < 0.99; // Because more than 0.99 means no object
+            return sampleDepth >= sampleDistance * 40.0  && sampleDepth < 0.9999; // Because more than 0.99 means no object
+        }
+
+		float adjustInputDepthToBeZeroToOne(float depth) {
+			//return depth;
+            return clamp((1.0 / (expectedMax - expectedMin)) * (depth - expectedMin), 0.0, 1.0);
         }
 
 		void main() {
 
 			vec4 inputColor = texture2D(tDiffuse, vUv);
 
-			vec4 color = vec4( 0.0 );
+			float color = 0.0;
 
 			float total = 0.0;
 
-			float influenceOfDarkness = (1.0 - color.a); // 0.0=not influenced, 1.0=very influenced
+			float influenceOfDarkness = (1.0 - color); // 0.0=not influenced, 1.0=very influenced
 
 			for (float y = -ITERATIONS; y <= ITERATIONS; y++) {
                 for (float x = -ITERATIONS; x <= ITERATIONS; x++) {
 					vec2 offset = delta * vec2(x, y);
 					vec4 sampleColor = texture2D(tDiffuse, vUv + offset);
-					float sampleDistance = length(offset);
-					float sampleDepth = max(0.0, 0.99 - sampleColor.a); // 0.0=near, 1.0=far
-					float weight = isInfluencingCurrentSample(sampleDistance, sampleDepth) ? 1.0 : 0.0; // sampleDepth > 0.99 ? 0.0 : 1.0; //gaussian(length(offset) * (1.0 + sampleDepth * (influenceOfDarkness * FACTOR_BETWEEN_DARK_AND_BRIGHT)), sigma);
+					float sampleDistance = abs(offset.x) + abs(offset.y);// length(offset);
+					float sampleDepth =adjustInputDepthToBeZeroToOne(1.0 - sampleColor.a); // 0.0=near, 1.0=far
+					
+					float weight =  isInfluencingCurrentSample(sampleDistance, sampleDepth) ? 0.005 / (sampleDepth * sampleDepth) : 0.0;// isInfluencingCurrentSample(sampleDistance, sampleDepth) ? 1.0 : 0.0; // sampleDepth > 0.99 ? 0.0 : 1.0; //gaussian(length(offset) * (1.0 + sampleDepth * (influenceOfDarkness * FACTOR_BETWEEN_DARK_AND_BRIGHT)), sigma);
 					//color += sampleColor * weight;
-					color += 1.0 * weight * sqrt(1.0 - sampleDepth);
+					color += weight * 0.5;
 					total += 1.0;
                 }
             }
 
-			vec4 resultingColor = color / total;
+			float resultingColor = clamp(color , 0.0, 1.0);
 
-			float adjustedAlpha = resultingColor.a; //pow(resultingColor.a, 0.7);
-			vec4 adjusted = vec4(0.0, 0.0, 0.0, inputColor.a);
+			float adjustedAlpha = resultingColor; //pow(resultingColor.a, 0.7);
+			vec4 adjusted = vec4(0.0, 0.0, 0.0, adjustedAlpha);
+
+			//vec4 adjusted = vec4(0.0, 0.0, 0.0, 1.0 - adjustInputDepthToBeZeroToOne(1.0 - inputColor.a));
 
 			gl_FragColor = adjusted;
 			
